b47de980525c742c72fa3d6fa9a8b7336e277b4a
hongaah <hongaah@qq.com>
2025-04-03 15:56:22 +0800

    feat: ignore activemq

--- java-notes/springboot/springboot 2/springboot-demo/src/main/java/cn/itsource/springbootdemo/SpringbootDemoApplication.java
+++ java-notes/springboot/springboot 2/springboot-demo/src/main/java/cn/itsource/springbootdemo/SpringbootDemoApplication.java
@@ -2,8 +2,17 @@ package cn.itsource.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+// activemq 的包不参与扫描，因为该配置需要本地启动 activemq
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = "cn.itsource.springbootdemo.projects.ActiveMQ.*"
+        )
+)
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
